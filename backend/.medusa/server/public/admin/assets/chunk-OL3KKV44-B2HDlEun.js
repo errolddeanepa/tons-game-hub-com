151bd342393825e96505e14a752eb90225161c6f
erroldepa <errold.epa@gmail.com>
2025-10-24 13:10:12 +0800

    add files

--- backend/.medusa/server/public/admin/assets/chunk-OL3KKV44-B2HDlEun.js
+++
@@ -1,1 +0,0 @@
-import{aO as i}from"./index-CBYBwu-X.js";var a=i("product","*categories,*shipping_profile,-variants");export{a as P};
---
+++ backend/.medusa/server/public/admin/assets/chunk-QJ6SBVJ2-DqeTl98d.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-B3O0k4xg.js";var a=()=>r.jsx("div",{style:{background:"repeating-linear-gradient(-45deg, rgb(212, 212, 216, 0.15), rgb(212, 212, 216,.15) 10px, transparent 10px, transparent 20px)"},className:"bg-ui-bg-field mt-4 block h-[56px] w-full rounded-lg border border-dashed"});export{a as I};
