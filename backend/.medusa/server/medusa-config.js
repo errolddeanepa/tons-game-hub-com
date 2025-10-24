2fdee1e1cf6a466caa31dbfcacc73bb14a5cd986
erroldepa <errold.epa@gmail.com>
2025-10-24 10:57:09 +0800

    modify config

--- backend/.medusa/server/medusa-config.js
+++ backend/.medusa/server/medusa-config.js
@@ -5,6 +5,8 @@ const utils_1 = require("@medusajs/framework/utils");
 module.exports = (0, utils_1.defineConfig)({
     projectConfig: {
         databaseUrl: process.env.DATABASE_URL,
+        redisUrl: process.env.REDIS_URL,
+        workerMode: process.env.MEDUSA_WORKER_MODE,
         http: {
             storeCors: process.env.STORE_CORS,
             adminCors: process.env.ADMIN_CORS,
@@ -12,6 +14,58 @@ module.exports = (0, utils_1.defineConfig)({
             jwtSecret: process.env.JWT_SECRET || "supersecret",
             cookieSecret: process.env.COOKIE_SECRET || "supersecret",
         }
-    }
+    },
+
+    admin: {
+    disable: process.env.DISABLE_MEDUSA_ADMIN === 'false',
+    },
+
+    modules: [
+        {
+          resolve: "@medusajs/medusa/caching",
+          options: {
+            providers: [
+              {
+                resolve: "@medusajs/caching-redis",
+                id: "caching-redis",
+                is_default: true,
+                options: {
+                  redisUrl: process.env.CACHE_REDIS_URL,
+                },
+              },
+            ],
+          },
+        },
+        {
+          resolve: "@medusajs/medusa/event-bus-redis",
+          options: {
+            redisUrl: process.env.REDIS_URL,
+          },
+        },
+        {
+          resolve: "@medusajs/medusa/workflow-engine-redis",
+          options: {
+            redis: {
+              url: process.env.REDIS_URL,
+            },
+          },
+        },
+        {
+          resolve: "@medusajs/medusa/locking",
+          options: {
+            providers: [
+              {
+                resolve: "@medusajs/medusa/locking-redis",
+                id: "locking-redis",
+                is_default: true,
+                options: {
+                  redisUrl: process.env.LOCKING_REDIS_URL,
+                },
+              },
+            ],
+          },
+        },
+    
+      ],
 });
 //# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWVkdXNhLWNvbmZpZy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL21lZHVzYS1jb25maWcudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSxxREFBaUU7QUFFakUsSUFBQSxlQUFPLEVBQUMsT0FBTyxDQUFDLEdBQUcsQ0FBQyxRQUFRLElBQUksYUFBYSxFQUFFLE9BQU8sQ0FBQyxHQUFHLEVBQUUsQ0FBQyxDQUFBO0FBRTdELE1BQU0sQ0FBQyxPQUFPLEdBQUcsSUFBQSxvQkFBWSxFQUFDO0lBQzVCLGFBQWEsRUFBRTtRQUNiLFdBQVcsRUFBRSxPQUFPLENBQUMsR0FBRyxDQUFDLFlBQVk7UUFDckMsSUFBSSxFQUFFO1lBQ0osU0FBUyxFQUFFLE9BQU8sQ0FBQyxHQUFHLENBQUMsVUFBVztZQUNsQyxTQUFTLEVBQUUsT0FBTyxDQUFDLEdBQUcsQ0FBQyxVQUFXO1lBQ2xDLFFBQVEsRUFBRSxPQUFPLENBQUMsR0FBRyxDQUFDLFNBQVU7WUFDaEMsU0FBUyxFQUFFLE9BQU8sQ0FBQyxHQUFHLENBQUMsVUFBVSxJQUFJLGFBQWE7WUFDbEQsWUFBWSxFQUFFLE9BQU8sQ0FBQyxHQUFHLENBQUMsYUFBYSxJQUFJLGFBQWE7U0FDekQ7S0FDRjtDQUNGLENBQUMsQ0FBQSJ9
