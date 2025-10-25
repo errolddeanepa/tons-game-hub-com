d901272b49a922bd8f1ce793d66e5c6fe5a59594
erroldepa <errold.epa@gmail.com>
2025-10-25 14:56:45 +0800

    add Modules to right path

--- backend/.medusa/server/medusa-config.js
+++ backend/.medusa/server/medusa-config.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const utils_1 = require("@medusajs/framework/utils");
-(0, utils_1.loadEnv,utils_1.Modules)(process.env.NODE_ENV || 'development', process.cwd());
-module.exports = (0, utils_1.defineConfig)({
+(0, utils_1.loadEnv)(process.env.NODE_ENV || 'development', process.cwd());
+module.exports = (0, utils_1.defineConfig, utils_1.Modules)({
     projectConfig: {
         workerMode: process.env.MEDUSA_WORKER_MODE,
         redisUrl: process.env.REDIS_URL,
