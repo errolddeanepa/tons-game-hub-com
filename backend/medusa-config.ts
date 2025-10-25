9f899e655b59a737291afe756e7febfccfcf58a1
erroldepa <errold.epa@gmail.com>
2025-10-25 23:11:38 +0800

    configure databasedriver

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -7,6 +7,7 @@ module.exports = defineConfig({
     workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
     redisUrl: process.env.REDIS_URL,
     databaseUrl: process.env.DATABASE_URL,
+    databaseDriverOptions: process.env.NODE_ENV !== "development" ? { connection: { ssl: { rejectUnauthorized: false } } } : {},
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
