71cfeba9bd329327d9b67eed9c0402c6f122164e
erroldepa <errold.epa@gmail.com>
2025-10-25 22:33:28 +0800

    add ./medusa ignore

--- backend/.medusa/server/public/admin/assets/chunk-F6ZOHZVB-BOv7MuVs.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-B3O0k4xg.js";var e=({children:s})=>r.jsx("span",{className:"sr-only",children:s});export{e as V};
--- backend/.medusa/server/public/admin/assets/chunk-OL3KKV44-BsnI9yTN.js
+++
@@ -1,1 +0,0 @@
-import{aO as i}from"./index-B3O0k4xg.js";var a=i("product","*categories,*shipping_profile,-variants");export{a as P};
